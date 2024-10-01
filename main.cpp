cb51e3874be328c9542cd83af3e67fa5c70cd77e
yokouchi_kouta <115063415+yokouchikouta@users.noreply.github.com>
2024-10-01 15:26:44 +0900

    プロジェクト ファイルを追加します。

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <cstdio>
+#include <cstdlib>
+
+int main(int argc, char* argv[]) {
+
+	for (int i = 0; i < argc; i++)
+	{
+		printf(argv[i]);
+		printf("\n");
+	}
+
+	system("pause");
+	return 0;
+}
